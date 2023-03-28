ed64f5b74db1569d2465579173795042a6ded3e6
Mattu <mateusz.pl671@gmail.com>
2023-03-28 20:09:14 +0200

    first commit

---
+++ Car-SharingDesktopAPP/Car-SharingDesktopAPP/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+using System.Diagnostics;
+
+namespace Car_SharingDesktopAPP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+        private void themeToggle_Click(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+            }
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+            }
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void ExitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (textUsername.Text == "admin")
+            {
+                Trace.WriteLine("Login successful");
+                MessageBox.Show("Login successful");
+                UserPanel userPanel = new UserPanel();
+                userPanel.Show();
+                this.Close();
+            }
+            else
+            {
+                Trace.WriteLine("Login failed");
+                MessageBox.Show("Login failed");
+            }
+        }
+    }
+}
