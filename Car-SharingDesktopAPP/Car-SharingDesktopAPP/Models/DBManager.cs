a9939164d82ff23d15ae2911d6354f326978828b
Mateusz "Mattu" Partyka <mateusz.pl671@gmail.com>
2023-04-09 13:19:26 +0200

    Add JSON file handling

---
+++ Car-SharingDesktopAPP/Car-SharingDesktopAPP/Models/DBManager.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_SharingDesktopAPP
+{
+    class DBManager
+    {
+        private static object connectionString;
+
+        public static object GetConnectionString()
+        {
+            string json = File.ReadAllText("./db.json");
+            var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            if(data.TryGetValue("ConnectionString", out connectionString))
+                return connectionString;
+
+            throw new Exception("ConnectionString not found in JSON file");
+        }
+    }
+}
